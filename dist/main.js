5ac9a0df83e92dab7ac103b8eb346bd09266556b
Santiago Garcia O. Dasago250 <Dasago250@gmail.com>
2023-03-04 15:34:55 -0500

    Basic page structure

--- dist/main.js
+++ dist/main.js
@@ -6,24 +6,27 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/** *** */ (() => { // webpackBootstrap
-/** *** */ 	const __webpack_modules__ = ({
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-    /***/ './src/index.js':
-    /*! **********************!*\
+/***/ "./src/index.js":
+/*!**********************!*\
   !*** ./src/index.js ***!
-  \********************* */
-    /***/ (() => {
-      eval('\n\n//# sourceURL=webpack://todo-list/./src/index.js?');
-      /***/ }),
+  \**********************/
+/***/ (() => {
 
-    /** *** */ 	});
-  /** ********************************************************************* */
-  /** *** */
-  /** *** */ 	// startup
-  /** *** */ 	// Load entry module and return exports
-  /** *** */ 	// This entry module can't be inlined because the eval devtool is used.
-  /** *** */ 	const __webpack_exports__ = {};
-  /** *** */ 	__webpack_modules__['./src/index.js']();
-/** *** */
-/** *** */ })();
+eval("\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
