8c09f871afb581c11a55a8870a516c31010669ae
Santiago Garcia O. Dasago250 <Dasago250@gmail.com>
2023-03-04 21:55:05 -0500

    Initial edit task logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import { tabCreation } from './tabCreation';
 import { taskCreation } from './taskCreation';
 
-const main = document.querySelector('.mainContent');
 const inboxTab = tabCreation('Inbox');
+const projectTestTab = tabCreation('Project Test');
 
+const main = document.querySelector('.mainContent');
+const inbox = document.querySelector('.inbox');
+const projectTest = document.querySelector('.projectTest');
+
+// Logic of tab selection
 main.appendChild(inboxTab.content);
+
+inbox.addEventListener('click', () => {
+  main.removeChild(main.lastChild);
+  main.appendChild(inboxTab.content);
+});
+
+projectTest.addEventListener('click', () => {
+  main.removeChild(main.lastChild);
+  main.appendChild(projectTestTab.content);
+});
 
 const taskModal = document.querySelector('.modal-task');
 
-const taskList = document.querySelector('.task-list');
-
+// Logic of buttons in the main content
 main.addEventListener('click', (e) => {
+  // Add task
   if (e.target.classList.contains('addTask')) {
     taskModal.showModal();
   }
+  // Delete Task
   if (e.target.classList.contains('task-delete')) {
     e.target.parentNode.remove();
   }
@@ -29,12 +45,13 @@ const taskModalAdd = document.querySelector('.taskModalAdd');
 const taskModalCancel = document.querySelector('.taskModalCancel');
 
 taskModalAdd.addEventListener('click', () => {
+  const taskList = document.querySelector('.task-list');
   const name = taskModalName.value;
   const description = taskModalDescription.value;
   const date = taskModalDate.value;
   const priority = taskModalPriority.value;
   const task = taskCreation(name, description, date, priority);
-  taskList.appendChild(task.taskContent);
+  taskList.appendChild(task.listElement);
   taskModalName.value = '';
   taskModalDescription.value = '';
   taskModalDate.value = '';
@@ -46,4 +63,38 @@ taskModalCancel.addEventListener('click', () => {
   taskModalDescription.value = '';
   taskModalDate.value = '';
   taskModalPriority.value = 1;
+});
+
+// Edit TASK
+
+main.addEventListener('click', (e) => {
+  if (e.target.classList.contains('task-edit')) {
+    const taskEditForm = e.target.parentNode.nextElementSibling;
+    if (taskEditForm.style.display === 'block') {
+      taskEditForm.style.display = 'none';
+    } else {
+      taskEditForm.style.display = 'block';
+    }
+  }
+  if (e.target.classList.contains('task-edit-confirm')) {
+    const currentTask = e.target.parentNode.parentNode.firstChild;
+    const taskEditName = e.target.parentNode.children[0];
+    const taskEditDescription = e.target.parentNode.children[1];
+    const taskEditDate = e.target.parentNode.children[2];
+    const taskEditPriority = e.target.parentNode.children[3];
+    if (taskEditName.value !== '') {
+      currentTask.children[0].textContent = taskEditName.value;
+    }
+    if (taskEditDescription.value !== '') {
+      currentTask.children[1].textContent = taskEditDescription.value;
+    }
+    if (taskEditDate.value !== '') {
+      currentTask.children[2].textContent = taskEditDate.value;
+    }
+    currentTask.style.backgroundColor = taskEditPriority.value;
+    taskEditName.value = '';
+    taskEditDescription.value = '';
+    taskEditDate.value = '2023-01-01';
+    e.target.parentNode.style.display = 'none';
+  }
 });
--- src/taskCreation.js
+++ src/taskCreation.js
@@ -1,6 +1,9 @@
 export const taskCreation = (name, description, date, priority) => {
-  const taskContent = document.createElement('li');
-  taskContent.classList.add('task');
+  // Main content of task object'
+  const listElement = document.createElement('li');
+  listElement.classList.add('task');
+  const taskContent = document.createElement('div');
+  taskContent.classList.add('task-content');
   const taskName = document.createElement('h4');
   taskName.classList.add('task-name');
   taskName.textContent = name;
@@ -11,14 +14,63 @@ export const taskCreation = (name, description, date, priority) => {
   taskDate.classList.add('task-date');
   taskDate.textContent = date;
   taskContent.style.backgroundColor = `${priority}`;
+  const taskEdit = document.createElement('button');
+  taskEdit.classList.add('task-edit');
+  taskEdit.textContent = 'edit';
   const taskDelete = document.createElement('button');
   taskDelete.classList.add('task-delete');
   taskDelete.textContent = 'delete';
 
+  // Edit task FORM ---------------------------------------------------
+  const taskEditContent = document.createElement('div');
+  taskEditContent.classList.add('task-edit-content');
+  const taskEditName = document.createElement('input');
+  taskEditName.setAttribute('type', 'text');
+  taskEditName.setAttribute('name', 'taskEditName');
+  taskEditName.setAttribute('id', 'taskEditName');
+  taskEditName.setAttribute('placeholder', 'name');
+  const taskEditDescription = document.createElement('input');
+  taskEditDescription.setAttribute('type', 'text');
+  taskEditDescription.setAttribute('name', 'taskEditDescription');
+  taskEditDescription.setAttribute('id', 'taskEditDescription');
+  taskEditDescription.setAttribute('placeholder', 'description');
+  const taskEditDate = document.createElement('input');
+  taskEditDate.setAttribute('type', 'date');
+  taskEditDate.setAttribute('name', 'taskEditDate');
+  taskEditDate.setAttribute('id', 'taskEditDate');
+  taskEditDate.value = '2023-01-01';
+  const taskEditPriority = document.createElement('select');
+  taskEditPriority.setAttribute('name', 'taskEditPriority');
+  taskEditPriority.setAttribute('id', 'taskEditPriority');
+  const colorPriority = ['#2F58CD', '#03C988', '#FFED00', '#F99417', '#FF0303'];
+  for (let i = 0; i < colorPriority.length; i += 1) {
+    const option = document.createElement('option');
+    option.setAttribute('value', colorPriority[i]);
+    option.textContent = `${i + 1}`;
+    taskEditPriority.appendChild(option);
+  }
+  const taskEditAdd = document.createElement('button');
+  taskEditAdd.classList.add('task-edit-confirm');
+  taskEditAdd.textContent = 'confirm';
+  const taskEditDelete = document.createElement('button');
+  taskEditDelete.classList.add('task-edit-cancel');
+  taskEditDelete.textContent = 'cancel';
+
+  taskEditContent.appendChild(taskEditName);
+  taskEditContent.appendChild(taskEditDescription);
+  taskEditContent.appendChild(taskEditDate);
+  taskEditContent.appendChild(taskEditPriority);
+  taskEditContent.appendChild(taskEditAdd);
+  taskEditContent.appendChild(taskEditDelete);
+
+  // Append all elements to li root element --------------------------------
   taskContent.appendChild(taskName);
   taskContent.appendChild(taskDescription);
   taskContent.appendChild(taskDate);
+  taskContent.appendChild(taskEdit);
   taskContent.appendChild(taskDelete);
 
-  return { taskContent };
+  listElement.appendChild(taskContent);
+  listElement.appendChild(taskEditContent);
+  return { listElement, taskContent };
 };
